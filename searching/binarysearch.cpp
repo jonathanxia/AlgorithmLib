e24d4a4fd31815268879a88c2d6628cd115dcc03
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-05 13:07:02 -0700

    Commentary and exception handling in binary search

--- searching/binarysearch.cpp
+++ searching/binarysearch.cpp
@@ -1,6 +1,31 @@
+#include<stdexcept>
+/**
+*   int binarysearch(cont A, T key)
+*
+*   Searches a sorted container A for an element key
+*   Template parameters:
+*       cont: the container that contains the elements, of type T
+*       T: the type of the elements in the container
+*   Parameters:
+        A: the container that has the elements of type T
+        key: the element to be found
+*   Returns:
+        the index the element key is at. Returns -1 if doesn't exist.
+*   Exceptions:
+*       std::invalid_argument: "Input container is not sorted"
+*/
 template <typename cont, typename T>
 int binarysearch(cont A, T key)
 {
+    // Check if A is sorted:
+
+    for(int i = 1; i < A.size(); i++)
+    {
+        if(A[i-1] > A[i])
+        {
+            throw std::invalid_argument("Input container is not sorted");
+        }
+    }
     // We are searching for the key in between A[lowerInd] and A[upperInd - 1]
     // The midpoint will be the average, and then we shall confine our search there.
     int lowerInd = 0;
