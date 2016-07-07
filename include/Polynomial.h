12d40b1a5fc06f694ddc90761fde3c9be8734852
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-07 00:22:32 -0700

    Created the polynomial class
    Addition and subtraction for polynomials.

--- include/Polynomial.h
+++ include/Polynomial.h
@@ -2,17 +2,36 @@
 #define POLYNOMIAL_H
 
 #include <vector>
+#include <initializer_list>
 
+template <typename coeff>
 class Polynomial
 {
     public:
         Polynomial(); // Constructs the 0 polynomial
-        ~Polynomial();
+        Polynomial(std::initializer_list<coeff> args);
+        Polynomial(std::vector<coeff> coefficients);
+
+        int getDegree() const;
+        std::vector<coeff> getCoefficients() const;
+
+        coeff operator() (coeff input) const;
+        Polynomial<coeff> operator+ (const Polynomial<coeff>& other) const;
+        Polynomial<coeff> operator- (const Polynomial<coeff>& other) const;
+        Polynomial<coeff> operator* (const Polynomial<coeff>& other) const;
+
+        Polynomial<coeff> operator+ (coeff constant);
+        Polynomial<coeff> operator- (coeff constant);
+        Polynomial<coeff> operator* (coeff constant);
+        virtual ~Polynomial();
 
     protected:
 
     private:
+        std::vector<coeff> mCoeff;
 
 };
+
+#include "../src/Polynomial.cpp"
 
 #endif // POLYNOMIAL_H
--- src/Polynomial.cpp
+++ src/Polynomial.cpp
@@ -1,11 +1,89 @@
-#include "Polynomial.h"
+#include <initializer_list>
 
-Polynomial::Polynomial()
+template <typename coeff>
+Polynomial<coeff>::Polynomial() :
+    mCoeff(1) //mCoeff is the vector storing the coefficients, constructs a 0 polynomial
 {
-    //ctor
+
 }
 
-Polynomial::~Polynomial()
+template <typename coeff>
+Polynomial<coeff>::Polynomial(std::initializer_list<coeff> args)
+{
+    mCoeff.reserve(args.size());
+    for(auto c : args)
+    {
+        mCoeff.push_back(c);
+    }
+}
+
+template <typename coeff>
+Polynomial<coeff>::Polynomial(std::vector<coeff> coefficients) :
+    mCoeff(coefficients)
+{
+
+}
+
+template <typename coeff>
+int Polynomial<coeff>::getDegree() const
+{
+    return mCoeff.size() - 1;
+}
+
+template <typename coeff>
+std::vector<coeff> Polynomial<coeff>::getCoefficients() const
+{
+    return mCoeff;
+}
+
+template <typename coeff>
+coeff Polynomial<coeff>::operator() (coeff input) const
+{
+    coeff sum = mCoeff[mCoeff.size() - 1];
+    for(int i = mCoeff.size() - 2; i >= 0; i--)
+    {
+        sum *= input;
+        sum += mCoeff[i];
+    }
+    return sum;
+}
+
+template <typename coeff>
+Polynomial<coeff> Polynomial<coeff>::operator+(const Polynomial<coeff>& other) const
+{
+    std::vector<coeff> ret;
+    int i;
+    for(i = 0; i < other.mCoeff.size() && i < mCoeff.size(); i++)
+    {
+        ret.push_back(mCoeff[i] + other.mCoeff[i]);
+    }
+    while (i < mCoeff.size())
+    {
+        ret.push_back(mCoeff[i]);
+        i++;
+    }
+    while (i < other.mCoeff.size())
+    {
+        ret.push_back(other.mCoeff[i]);
+        i++;
+    }
+    return ret;
+}
+
+template <typename coeff>
+Polynomial<coeff> Polynomial<coeff>::operator-(const Polynomial<coeff>& other) const
+{
+    std::vector<coeff> ret = other.mCoeff;
+    for(int i = 0; i < ret.size(); i++)
+    {
+        ret[i] *= -1;
+    }
+    Polynomial<coeff> poly(ret);
+    return *this + poly;
+}
+
+template <typename coeff>
+Polynomial<coeff>::~Polynomial()
 {
     //dtor
 }
