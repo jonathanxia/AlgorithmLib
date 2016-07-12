57ded22d64ab06c0433a0b34f7eb076401b02f48
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-11 23:47:44 -0700

    GCD and LCM static functions implemented

--- include/Fraction.h
+++ include/Fraction.h
@@ -1,29 +1,37 @@
 #ifndef FRACTION_H
 #define FRACTION_H
 
-
-class Fraction
+namespace AlgLib
 {
-    public:
-        /** Default constructor */
-        Fraction();
-        /** Default destructor */
-        virtual ~Fraction();
-        /** Copy constructor
-         *  \param other Object to copy from
-         */
-        Fraction(const Fraction& other);
-        /** Assignment operator
-         *  \param other Object to assign from
-         *  \return A reference to this
-         */
-        Fraction& operator=(const Fraction& other);
+    class Fraction
+    {
+        public:
+            /** Default constructor */
+            Fraction();
+            /** Default destructor */
+            virtual ~Fraction();
+            /** Copy constructor
+             *  \param other Object to copy from
+             */
+            Fraction(const Fraction& other);
+            /** Assignment operator
+             *  \param other Object to assign from
+             *  \return A reference to this
+             */
+            Fraction& operator=(const Fraction& other);
 
-    protected:
+            Fraction(int n, int d);
 
-    private:
-        int mNumerator = 0;
-        int mDenominator = 1;
-};
+            static int gcd(int a, int b); // takes the gcd of two numbers
+            static int lcm(int a, int b);
+
+        protected:
+
+        private:
+            int mNumerator = 0;
+            int mDenominator = 1;
+    };
+
+}
 
 #endif // FRACTION_H
--- src/Fraction/Fraction.cpp
+++ src/Fraction/Fraction.cpp
@@ -1,28 +1,63 @@
 #include "Fraction.h"
 
-Fraction::Fraction() :
+AlgLib::Fraction::Fraction() :
     mNumerator(0),
     mDenominator(1)
 {
     //ctor
 }
 
-Fraction::~Fraction()
+AlgLib::Fraction::~Fraction()
 {
     //dtor
 }
 
-Fraction::Fraction(const Fraction& other)
+AlgLib::Fraction::Fraction(const Fraction& other)
 {
     mNumerator = other.mNumerator;
     mDenominator = other.mDenominator;
 }
 
-Fraction& Fraction::operator=(const Fraction& rhs)
+AlgLib::Fraction& AlgLib::Fraction::operator=(const Fraction& rhs)
 {
     if (this == &rhs) return *this; // handle self assignment
     //assignment operator
     mNumerator = rhs.mNumerator;
     mDenominator = rhs.mDenominator;
     return *this;
+}
+
+/** @brief (one liner)
+  *
+  * (documentation goes here)
+  */
+AlgLib::Fraction::Fraction(int n, int d) :
+    mNumerator(n),
+    mDenominator(d)
+{
+
+}
+
+int AlgLib::Fraction::gcd(int a, int b)
+{
+    // Now we use the Euclidean algorithm
+    if(a < b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp; // Does the switcheroo
+    }
+    if(b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int AlgLib::Fraction::lcm(int a, int b)
+{
+    return a * b / gcd(a, b);
 }
