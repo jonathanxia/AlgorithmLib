e1935a4e98cdc4652c8d5ae353967f3f7471444b
Tae Hyung Kim <thkim1011@hotmail.com>
2016-07-14 17:55:30 -0700

    Added Matrix files

--- Matrix.h
+++ Matrix.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include<initializer_list>
+#include<vector>
+
+namespace AlgLib {
+	template<unsigned int R, unsigned int C>
+	class Matrix {
+		Matrix();
+		Matrix(std::initializer_list<std::initializer_list<int> >); // Probably should add more constructors later
+
+		
+		
+	};
+}
+
+#endif
