0dc6da3e0e5136ba839fded58d191f00131acc60
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-17 12:50:57 -0700

    Put everything in the AlgLib namespace

--- include/sorting.h
+++ include/sorting.h
@@ -3,14 +3,17 @@
 #include "../sorting/insertionsort.cpp"
 #include "../sorting/mergesort.cpp"
 #include "../sorting/heapsort.cpp"
-template <typename T>
-void insertionSort(T& arr);
+namespace AlgLib
+{
+    template <typename T>
+    void insertionSort(T& arr);
 
-template <typename T>
-void mergeSort(T& arr);
-template <typename T>
-T MERGE(T& left, T& right);
+    template <typename T>
+    void mergeSort(T& arr);
+    template <typename T>
+    T MERGE(T& left, T& right);
 
-template <typename T, typename S>
-void heapSort(T & arr);
+    template <typename T, typename S>
+    void heapSort(T & arr);
+}
 #endif // SORTING_H
--- searching/binarysearch.cpp
+++ searching/binarysearch.cpp
@@ -1,44 +1,47 @@
 #include<stdexcept>
-/**
-*   int binarysearch(cont A, T key)
-*
-*   Searches a sorted container A for an element key
-*   Template parameters:
-*       cont: the container that contains the elements, of type T
-*       T: the type of the elements in the container
-*   Parameters:
-        A: the container that has the elements of type T
-        key: the element to be found
-*   Returns:
-        the index the element key is at. Returns -1 if doesn't exist.
-*/
-template <typename cont, typename T>
-int binarysearch(cont A, T key)
+namespace AlgLib
 {
-    // We are searching for the key in between A[lowerInd] and A[upperInd - 1]
-    // The midpoint will be the average, and then we shall confine our search there.
-    int lowerInd = 0;
-    int upperInd = A.size();
-    int midpoint = (lowerInd + upperInd) / 2;
-
-    while(lowerInd != upperInd) //if lowerInd == midpoint, then that means lowerInd and upperInd differ by
-        //at most 1
+    /**
+    *   int binarysearch(cont A, T key)
+    *
+    *   Searches a sorted container A for an element key
+    *   Template parameters:
+    *       cont: the container that contains the elements, of type T
+    *       T: the type of the elements in the container
+    *   Parameters:
+            A: the container that has the elements of type T
+            key: the element to be found
+    *   Returns:
+            the index the element key is at. Returns -1 if doesn't exist.
+    */
+    template <typename cont, typename T>
+    int binarysearch(cont A, T key)
     {
-        if(key == A[midpoint])
-        {
-            return midpoint;
-        }
-        else if (key < A[midpoint]) //We constrain our search from lowerInd to midpoint
-        {
-            upperInd = midpoint;
-            midpoint = (lowerInd + upperInd) / 2;
-        }
-        else
-        {
-            lowerInd = midpoint;
-            midpoint = (lowerInd + upperInd) / 2;
-        }
-    }
+        // We are searching for the key in between A[lowerInd] and A[upperInd - 1]
+        // The midpoint will be the average, and then we shall confine our search there.
+        int lowerInd = 0;
+        int upperInd = A.size();
+        int midpoint = (lowerInd + upperInd) / 2;
 
-    return -1;
+        while(lowerInd != upperInd) //if lowerInd == midpoint, then that means lowerInd and upperInd differ by
+            //at most 1
+        {
+            if(key == A[midpoint])
+            {
+                return midpoint;
+            }
+            else if (key < A[midpoint]) //We constrain our search from lowerInd to midpoint
+            {
+                upperInd = midpoint;
+                midpoint = (lowerInd + upperInd) / 2;
+            }
+            else
+            {
+                lowerInd = midpoint;
+                midpoint = (lowerInd + upperInd) / 2;
+            }
+        }
+
+        return -1;
+    }
 }
