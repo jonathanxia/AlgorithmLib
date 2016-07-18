b2529b377e7c33844971875ae9f7ceaa864cd658
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-18 16:46:09 -0700

    Fixed up the Matrix(int r, int c) constructor

--- include/Matrix.h
+++ include/Matrix.h
@@ -14,14 +14,19 @@ namespace AlgLib {
 		Matrix(int row, int column);
 		Matrix(std::initializer_list<std::initializer_list<T> > m); // Probably should add more constructors later
 
-		Matrix operator*(const Matrix& other) const;
-		Matrix operator+(const Matrix& other) const;
-		Matrix operator-(const Matrix& other) const;
-		Matrix inverse();
-		Matrix transpose();
-		int determinant();
+		/**
+		* Copy constructor
+		*/
+		Matrix(const Matrix& rhs);
 
-		void toString(); // Temporary solution to viewing... i just found out that you can return an output stream object
+		/*virtual Matrix operator*(const Matrix& other) const;
+		virtual Matrix operator+(const Matrix& other) const;
+		virtual Matrix operator-(const Matrix& other) const;
+		virtual Matrix inverse();
+		virtual Matrix transpose();
+		virtual int determinant(); */
+
+		//virtual void toString(); // Temporary solution to viewing... i just found out that you can return an output stream object
 
 	private:
 		std::vector<std::vector<T>> mMatrix;
--- src/Matrix/Matrix.cpp
+++ src/Matrix/Matrix.cpp
@@ -1,55 +1,64 @@
 #include <vector>
 #include <string>
 #include <iostream>
+namespace AlgLib
+{
+    template <typename T>
+    Matrix<T>::Matrix(int row, int column) { // Creates 0 matrix
+        std::vector< std::vector<T> > rows;
+        rows.reserve(row);
+        for (int i = 0; i < row; i++) {
+            std::vector<T> currentRow; // The current row being built up
+            currentRow.reserve(column);
+            for (int j = 0; j < column; j++) {
+                currentRow.push_back(0);
+            }
+            rows.push_back(currentRow);
+        }
+        mMatrix = rows;
+    }
 
-template <typename T>
-AlgLib::Matrix<T>::Matrix(int r, int c) { // Creates 0 matrix
-	std::vector<T> rows;
-	rows.reserve(row);
-	for (int i = 0; i < row; i++) {
-		std::vector<T> row;
-		row.reserve(column);
-		for (int j = 0; j < column; j++) {
-			row.add(0);
-		}
-		rows.add(row);
-	}
-	mMatrix = rows;
-}
+    template<typename T>
+    Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T> > matrix) {
+        // Find r and c
+        int r = 0, c = 0;
+        for (auto i = matrix.begin(); i != matrix.end(); i++) {
+            r = i->size() > r ? i->size() : r;
+            c++;
+        }
+        // Create matrix
+        std::vector<std::vector<T>> m;
+        for (auto i = matrix.begin(); i != matrix.end(); i++) {
+            std::vector<T> row;
+            row.reserve(c);
+            int counter = 0;
+            for (auto j = i->begin(); j != i->end(); j++) {
+                row.push_back(*j);
+                counter++;
+            }
+            while (counter < c) {
+                row.push_back(T(0)); // derp idk if this is allowed..
+            }
+            m.push_back(row);
+        }
+        mMatrix = m;
+    }
 
-template<typename T>
-AlgLib::Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T> > matrix) {
-	// Find r and c
-	int r = 0, c = 0;
-	for (auto i = matrix.begin(); i != matrix.end(); i++) {
-		r = i->size() > r ? i->size() : r;
-		c++;
-	}
-	// Create matrix
-	std::vector<std::vector<T>> m;
-	for (auto i = matrix.begin(); i != matrix.end(); i++) {
-		std::vector<T> row;
-		row.reserve(c);
-		int counter = 0;
-		for (auto j = i->begin(); j != i->end(); j++) {
-			row.push_back(*j);
-			counter++;
-		}
-		while (counter < c) {
-			row.push_back(T(0)); // derp idk if this is allowed..
-		}
-		m.push_back(row);
-	}
-	mMatrix = m;
-}
+    template <typename T>
+    Matrix<T>::Matrix(const Matrix<T>& rhs) :
+        mMatrix(rhs.mMatrix)
+    {
 
-template<typename T>
-void AlgLib::Matrix<T>::toString() { // Temporary sol until i figure out how to return output streams
-	for (auto i = mMatrix.begin(); i != mMatrix.end(); i++) {
-		cout << "[ ";
-		for (auto j = i->begin(); j != i->end(); j++) {
-			cout << " " << (*j) << " ";
-		}
-		cout << " ]\n";
-	}
+    }
+    //Let's avoid the code below for now --Jonathan Xia 07/18/2016
+    /*template<typename T>
+    void Matrix<T>::toString() { // Temporary sol until i figure out how to return output streams
+        for (auto i = mMatrix.begin(); i != mMatrix.end(); i++) {
+            cout << "[ ";
+            for (auto j = i->begin(); j != i->end(); j++) {
+                cout << " " << (*j) << " ";
+            }
+            cout << " ]\n";
+        }
+    } */
 }
