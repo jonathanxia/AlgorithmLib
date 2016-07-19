a7f11afa842752b9a4d192d8f6b2bca990c3af75
Tae Hyung Kim <thkim1011@hotmail.com>
2016-07-19 12:22:32 -0700

    Added group class

---
+++ include/Group.h
@@ -0,0 +1,25 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include<Permutation.h>
+
+namespace AlgLib {
+	class GroupElement {
+	public:
+		GroupElement();
+		virtual ~GroupElement();
+		virtual GroupElement inverse();
+		virtual GroupElement compose(GroupElement other); // not overloading operator since groups can be both additive and multiplicative
+	};
+
+
+	template<typename T : GroupElement>
+	class Group {
+
+	};
+
+
+}
+
+
+#endif
