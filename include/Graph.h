2b3bad0096ef7c1c9f76f158817754ccc451449e
Jonathan Xia <fclvbfm934@gmail.com>
2016-07-19 00:24:54 -0700

    Added empty Graph class

---
+++ include/Graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+
+class Graph
+{
+    public:
+        /** Default constructor */
+        Graph();
+        /** Default destructor */
+        virtual ~Graph();
+
+    protected:
+
+    private:
+};
+
+#endif // GRAPH_H
