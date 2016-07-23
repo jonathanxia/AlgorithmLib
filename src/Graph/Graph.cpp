39e3d4e9987430aef03e7e1d29a0fa3ecddfe525
jonathanxia <jonathanxia@berkeley.edu>
2016-07-22 19:55:51 -0700

    Made things actually run.

--- src/Graph/Graph.cpp
+++ src/Graph/Graph.cpp
@@ -1,8 +1,9 @@
 #include "Graph.h"
-
+#include "adjMatrix.h"
+#include "adjList.h"
 namespace AlgLib
 {
-    Graph::Graph(int numVertices, dbType storageType = dbType::ADJACENCY_MATRIX)
+    Graph::Graph(int numVertices, dbType storageType)
     {
         if(storageType == dbType::ADJACENCY_MATRIX)
         {
