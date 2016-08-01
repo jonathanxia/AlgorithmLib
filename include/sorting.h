a497945ee73511239c21d460c9f392863615c8a2
jonathanxia <jonathanxia@berkeley.edu>
2016-07-31 17:32:14 -0700

    Added documentation for sorting.h

--- include/sorting.h
+++ include/sorting.h
@@ -5,14 +5,32 @@
 #include "../sorting/heapsort.cpp"
 namespace AlgLib
 {
+    /** \brief Performs an insertion sort on the container
+     *
+     * \param T The type of the container e.g. std::vector<int>
+     * \param arr The container to be sorted
+     *
+     */
     template <typename T>
     void insertionSort(T& arr);
 
+    /** \brief Performs a merge sort on the container
+     *
+     * \param T The type of the container e.g. std::vector<int>
+     * \param arr The container to be sorted
+     *
+     */
     template <typename T>
     void mergeSort(T& arr);
     template <typename T>
     T MERGE(T& left, T& right);
 
+    /** \brief Performs a heap sort on the container
+     *
+     * \param T The type of the container e.g. std::vector<int>
+     * \param arr The container to be sorted
+     *
+     */
     template <typename T, typename S>
     void heapSort(T & arr);
 }
